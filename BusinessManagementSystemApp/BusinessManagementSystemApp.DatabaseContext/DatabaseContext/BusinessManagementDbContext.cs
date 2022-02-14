f89b5d0f17429bb0cdd71cee5b20a55c8412e114
TasmiaTasmin <tasmintasmia@gmail.com>
2022-02-14 15:23:36 +0600

    project added

---
+++ BusinessManagementSystemApp/BusinessManagementSystemApp.DatabaseContext/DatabaseContext/BusinessManagementDbContext.cs
@@ -0,0 +1,20 @@
+﻿using BusinessManagementSystemApp.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessManagementSystemApp.DatabaseContext.DatabaseContext
+{
+    public class BusinessManagementDbContext:DbContext
+    {
+        public BusinessManagementDbContext()
+        {
+            Configuration.LazyLoadingEnabled = false;
+        }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Order> Orders { get; set; }        
+    }
+}
